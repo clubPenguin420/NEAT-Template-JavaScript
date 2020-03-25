eaf16dc50e8697df79d72c4df608628843c142c2
clubPenguin420 <42702641+clubPenguin420@users.noreply.github.com>
2020-03-25 13:57:34 -0500

    Convert top variables

---
+++ Python/main.py
@@ -0,0 +1,22 @@
+# this is a template to add a NEAT ai to any game
+# note: <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<replace means that there is some information specific to the game to input here
+
+
+nextConnectionNo = 1000
+population = None
+speed = 60
+
+
+
+showBest = True #true if only show the best of the previous generation
+runBest = False #true if replaying the best ever game
+humanPlaying = False #true if the user is playing
+
+humanPlayer = None 
+
+
+showBestEachGen = False
+upToGen = 0
+genPlayerTemp = None #player
+showNothing = False
+
